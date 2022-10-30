e938ac60f521a3f9a493f0a76b82de01a338710b
Ben <benbugarindev@gmail.com>
2022-10-30 21:18:57 +0100

    readme update

--- src/component/PhaserGame.js
+++ src/component/PhaserGame.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Phaser from 'phaser';
 import LoginScene from '../scenes/LoginScene'; // Import our first scene
-import BasePlugin from '../plugin/BasePlugin';
+import BasePlugin from '../plugin/BasePlugin'; // Import our BasePlugin
 
 function PhaserGame() {
         new Phaser.Game({ 
@@ -12,11 +12,11 @@ function PhaserGame() {
                 autoCenter: Phaser.Scale.CENTER_HORIZONTALLY // We can set horrizontally our canvas
             },
             scene: [
-                LoginScene // The first scene is the Login Scene
+                LoginScene // Define our Scenes here, the first scene is the Login Scene
             ],
             plugins: {
                 global: [
-                    { key: 'BasePlugin', plugin: BasePlugin, start: false, mapping: 'baseplugin'},
+                    { key: 'BasePlugin', plugin: BasePlugin, mapping: 'baseplugin'}, // Pass our plugin with a key and a mapping (for calling our functions)
                 ],
             },
             parent: 'game-content',
